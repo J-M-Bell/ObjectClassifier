2dde1d2ad02a487bb62b28ff7035458ff1e40713
JM Bell <justinmbell1997@gmail.com>
2025-12-22 14:39:03 -0600

    General - experimented with both shallow and deep neural nets with high amount of parameters but not exceeding 100,000; will look into CNNs next

--- ObjectClassifier.py
+++ ObjectClassifier.py
@@ -11,12 +11,12 @@ from sklearn.model_selection import train_test_split
 
 # Data Preparation and Processing
 data = cifar10.load_data()
-(X_trainset, y_trainset), (X_test, y_test) = cifar10.load_data()
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
 
-X_trainset = X_trainset / 255
+X_train = X_train / 255
 X_test = X_test / 255
 
-X_train, X_validation, y_train, y_validation = train_test_split(X_trainset, y_trainset, test_size=0.2, random_state=0)
+# X_train, X_validation, y_train, y_validation = train_test_split(X_trainset, y_trainset, test_size=0.2, random_state=0)
 
 # Shapes of data variables
 # print(X_trainset.shape)
@@ -31,10 +31,10 @@ X_train, X_validation, y_train, y_validation = train_test_split(X_trainset, y_tr
 #Target transformation
 output_encoder = LabelBinarizer()
 y_train = output_encoder.fit_transform(y_train)
-y_validation = output_encoder.transform(y_validation)
+# y_validation = output_encoder.transform(y_validation)
 y_test = output_encoder.transform(y_test)
 
-# print(X_train.shape)
+print(X_train.shape)
 # print(X_validation.shape)
 # print(X_test.shape)
 # print(y_train.shape)
@@ -51,16 +51,23 @@ print(X_train.shape[1:2])
 model = Sequential([
     Input(shape=X_train.shape[1:4]),
     Flatten(),
-    Dense(units=4, activation=activations.sigmoid),
-    Dense(units=y_train.shape[1])
+    Dense(units=2, activation=activations.relu),
+    Dense(units=2000, activation=activations.relu),
+    Dense(units=4, activation=activations.relu),
+    Dense(units=1500, activation=activations.relu),
+    Dense(units=6, activation=activations.relu),
+    Dense(units=1000, activation=activations.relu),
+    Dense(units=20, activation=activations.relu),
+    Dense(units=1000, activation=activations.relu),
+    Dense(units=y_train.shape[1], activation=activations.softmax)
 ])
 print(model.summary())
 
 
 callbacks = []
-checkpoint = ModelCheckpoint("weights/Test 2 (4)/5-4000-{loss:.4f}" + ".weights.h5", monitor='loss', verbose=1,
-                                         save_best_only=True, save_weights_only=True, mode='min')
+checkpoint = ModelCheckpoint("weights/2-2000-4-1500-6-1000-20-1000/100-4000-{loss:.4f}" + ".keras", monitor='loss', verbose=1,
+                                         save_best_only=True, mode='min')
 callbacks = [checkpoint]
-model.load_weights("weights/Test 2 (4)/5-4000-5.3198.weights.h5")
-model.compile(optimizer='adam', loss=CategoricalCrossentropy())
-model.fit(X_train, y_train, epochs=5, batch_size=4000, shuffle=False, callbacks=callbacks, verbose=1)
+# model.load_weights("weights/Test 3 (20)/100-4000-2.0240.keras")
+model.compile(optimizer='adam', loss=CategoricalCrossentropy(), metrics=['accuracy'])
+model.fit(X_train, y_train, epochs=100, batch_size=4000, shuffle=False, callbacks=callbacks, verbose=1, validation_split=0.1)

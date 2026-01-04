f694b88ca2bcb832b997e21a7dfcc5ef2b8af6c7
JM Bell <justinmbell1997@gmail.com>
2026-01-03 18:20:31 -0600

    ClassifierModel (Mobile) - add documentation to model file

--- ObjectClassifier/ObjectClassifier/Model/ClassifierModel.swift
+++ ObjectClassifier/ObjectClassifier/Model/ClassifierModel.swift
@@ -9,9 +9,13 @@ import SwiftUI
 import Foundation
 import CoreML
 
+/// A struct model that holds UI Model class for the ClassifierView
 struct ClassifierModel {
     
+    /// list of targets from the model
     let classes = ["Airplane", "Automobile", "Bird", "Cat", "Deer", "Dog", "Frog", "Horse", "Ship", "Truck"]
+    
+    /// Object Classifier model object
     let model: ObjectClassifierModel = {
         do {
             let config = MLModelConfiguration()
@@ -21,6 +25,8 @@ struct ClassifierModel {
             fatalError("Error loading model")
         }
     }()
+    
+    /// image to be inputted to model
     var image: UIImage?
     
     

bd6b50d393f0cf8df8eb0a749d308bfdec5dc791
JM Bell <justinmbell1997@gmail.com>
2026-01-04 13:04:30 -0600

    starting app unit testing

---
+++ ObjectClassifier/ObjectClassifierTests/ClassifierViewModelTests.swift
@@ -0,0 +1,29 @@
+//
+//  ObjectClassifierTests.swift
+//  ObjectClassifierTests
+//
+//  Created by Justin Bell on 12/28/25.
+//
+
+import Testing
+@testable import ObjectClassifier
+
+class ClassifierViewModelTests {
+
+    
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+    
+    @Test func processImageTest() {
+        // Create VM object
+        var vm = ClassifierViewModel()
+        
+        //import black image
+        //test function off of image
+            
+            //call function
+        
+    }
+
+}
